77aaf5a92061a37e51acdf8037a16e85ca33f1f7
Jetro <lexgodesarrollo2@gmail.com>
2024-04-19 01:52:27 -0600

    uploads

--- assets/js/rest.js
+++ assets/js/rest.js
@@ -1,7 +1,41 @@
 
-function getdata(){
-    fetch('http://localhost:9001/api/listfiles')
-      .then(response => response.json())
-      .then(json => console.log(json))
+// function getdata(){
+//     fetch('http://3.14.68.118:9001/api/listfiles')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+// }
+
+function getFiles() {
+  const fileInput = document.getElementById('formFile');
+  const files = fileInput.files;
+
+  if (files.length === 0) {
+    console.log('No se han seleccionado archivos.');
+    return;
+  }
+
+  for (let i = 0; i < files.length; i++) {
+    console.log(files[i]);
+    uploadFile(files[i]);
+  }
 }
-getdata()
+function uploadFile(file) {
+  const formData = new FormData();
+  formData.append('files', file);
+
+  fetch('http://3.14.68.118:9001/api/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    console.log('Archivo subido con éxito:', data);
+    window.location.href = 'index.html'
+  })
+  .catch(error => {
+    console.error('Error al subir el archivo:', error);
+  });
+}
+
